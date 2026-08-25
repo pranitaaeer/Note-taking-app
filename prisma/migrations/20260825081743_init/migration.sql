4eaaa2fd96f5c16abbe6214f47310043464928df
pranitaaeer <aeerpranita@gmail.com>
2026-08-25 14:19:58 +0530

    add prisma schema and migration files

---
+++ prisma/migrations/20260825081743_init/migration.sql
@@ -0,0 +1,73 @@
+-- CreateEnum
+CREATE TYPE "ShareType" AS ENUM ('ONE_TIME', 'TIME_BASED');
+
+-- CreateEnum
+CREATE TYPE "AccessType" AS ENUM ('PUBLIC', 'PASSWORD');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShareLink" (
+    "id" TEXT NOT NULL,
+    "noteId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "shareType" "ShareType" NOT NULL,
+    "accessType" "AccessType" NOT NULL,
+    "accessKeyHash" TEXT,
+    "expiresAt" TIMESTAMP(3),
+    "usedAt" TIMESTAMP(3),
+    "revokedAt" TIMESTAMP(3),
+    "viewCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ShareLink_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "Note_userId_idx" ON "Note"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ShareLink_token_key" ON "ShareLink"("token");
+
+-- CreateIndex
+CREATE INDEX "ShareLink_noteId_idx" ON "ShareLink"("noteId");
+
+-- CreateIndex
+CREATE INDEX "ShareLink_token_idx" ON "ShareLink"("token");
+
+-- CreateIndex
+CREATE INDEX "ShareLink_expiresAt_idx" ON "ShareLink"("expiresAt");
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShareLink" ADD CONSTRAINT "ShareLink_noteId_fkey" FOREIGN KEY ("noteId") REFERENCES "Note"("id") ON DELETE CASCADE ON UPDATE CASCADE;
